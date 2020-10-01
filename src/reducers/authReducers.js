6ba7cb3ffcd098695817ed7fc4a6aab452070c75
MihaiGhe <mihai.md16@gmail.com>
2020-10-01 13:15:52 +0300

    CRUD Operations
    Hope it works

--- src/reducers/authReducers.js
+++ src/reducers/authReducers.js
@@ -1,9 +1,8 @@
-import { SIGN_IN, SIGN_OUT } from '../actions/types'
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
 
 const INITIAL_STATE = {
   isSignedIn: null,
-  userId: null
-
+  userId: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
